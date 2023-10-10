d1a27774a046f0efb37bb05b05e25b5cffce481c
alym karim <alymkarimg@gmail.com>
2023-10-11 00:25:12 +0100

    fix: move req log

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,8 @@ import JobUploadForm from '@/components/Forms/JobUploadForm';
 
 export const getServerSideProps = async (context: GetServerSidePropsContext) => {
   const { req } = context;
-  console.log('req', req)
   let url = req.headers.referer;
+  console.log('req', req)
   let arr = url!.split('/');
   url = `${arr[0]}//${arr[2]}`;
   const res = await fetch(`${url}/api/`);
