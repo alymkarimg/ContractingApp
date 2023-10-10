41af838b5a22261886201d4fbf0b247a7f1b5b92
alym karim <alymkarimg@gmail.com>
2023-10-11 00:51:18 +0100

    fix: change header

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import JobUploadForm from '@/components/Forms/JobUploadForm';
 export const getServerSideProps = async (context: GetServerSidePropsContext) => {
   const { req } = context;
   let url = req.headers.referer;
-  url =  process.env.NODE_ENV === 'production' ? req.headers['host'] : `${url!.split('/')[0]}//${url!.split('/')[2]}`;
+  url =  process.env.NODE_ENV === 'production' ? req.headers['x-vercel-deployment-url'] : `${url!.split('/')[0]}//${url!.split('/')[2]}`;
   const res = await fetch(`${url}/api/`);
   const data: IResponse = await res.json();
   return { props: { data } };
