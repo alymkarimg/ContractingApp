42a8cbfc201fe86fca7be67a66df4d3b169ba363
alym karim <alymkarimg@gmail.com>
2023-10-17 13:03:45 +0100

    fix: change @ import to relative path

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,8 @@
-import JobUploadForm from '@/components/forms/JobUploadForm';
+import JobUploadForm from '../components/forms/JobUploadForm';
 import { ToastContainer } from 'react-toastify';
 import Image from 'next/image';
-import { IResponse } from '@/interfaces/response.interface';
 
-export const getServerSideProps = async () => {
-  let url;
-  if (process.env.NODE_ENV !== 'production') {
-    url = 'http://localhost:3000';
-  } else {
-    url = `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
-  }
-  const res = await fetch(`${url}/api/`);
-  const data: IResponse = await res.json();
-  return { props: { data, apiKey: process.env.GOOGLE_API_KEY } };
-  return { props: { apiKey: process.env.GOOGLE_API_KEY } };
-};
-
-export default function Home({ apiKey }: { data: IResponse; apiKey: string }) {
+export default function Home({ apiKey }: { apiKey: string }) {
   return (
     <main>
       <div className="container">
