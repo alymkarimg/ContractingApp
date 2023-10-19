56b88dbb002b9c2a799c213d0825f4cd3cd8f3e1
alym karim <alymkarimg@gmail.com>
2023-10-19 14:12:35 +0100

    fix: remove IJobForm interface

--- interfaces/job.interface.ts
+++ interfaces/job.interface.ts
@@ -1,16 +1,3 @@
-export interface IJobForm {
-  title: string;
-  location: string;
-  address: string;
-  lat: number;
-  lng: number;
-  datetime__start?: string;
-  datetime__end?: string;
-  pay: number;
-  occupation: string;
-  description: string;
-}
-
 export interface IJob {
   _id: string;
   __v: string;
