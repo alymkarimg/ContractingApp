5e3bcb850506ff19493359bf582952de58d22bd4
alym karim <alymkarimg@gmail.com>
2023-10-20 04:42:14 +0100

    fix: exhuastive deps getjobs

--- pages/admin-area.tsx
+++ pages/admin-area.tsx
@@ -105,7 +105,7 @@ export default function AdminArea({ success }: { success?: string }) {
 
   useEffect(() => {
     getJobs();
-  }, []);
+  }, [getJobs]);
 
   useEffect(() => {
     if (success) {
